59ebc15973de6e0c81f879871b8ab7e2a99ff599
Goto Norio <gotonorio@gmail.com>
2021-06-08 22:28:32 +0900

    スマホ対応の修正

--- register/views.py
+++ register/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 
 from .forms import LoginForm
+from django.shortcuts import render
 
 
 class Login(LoginView):
@@ -13,3 +14,8 @@ class Login(LoginView):
 class Logout(LoginRequiredMixin, LogoutView):
     """ログアウトページ"""
     template_name = 'register/logout.html'
+
+
+# メニュー画面
+def warehouse_menu(request):
+    return render(request, 'register/menu.html')
