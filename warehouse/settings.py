ed2569abc496e72a48cf2090542d1df12e4b2a9f
Goto Norio <gotonorio@gmail.com>
2021-08-15 15:53:21 +0900

    SECRET_KEYのgithub対応

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '5sy@y2ual*=7x#1@i*i&(u-e6d331kqr5to4)vv-@!rvuc9wrc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
