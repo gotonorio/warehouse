f2687f4a0640d233b4720a22b81fca1c4c4aa098
Goto Norio <gotonorio@gmail.com>
2022-02-24 20:21:08 +0900

    メインページのタイトルをソフィア・ガーデンズ川崎に修正

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 CSRF_TRUSTED_ORIGINS = ['https://*.sophiagardens.org']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-VERSION_NO = '2022-02-21'
+VERSION_NO = '2022-02-24'
 # ファイルアップロードアプリuploder用
 # https://qiita.com/okoppe8/items/86776b8df566a4513e96
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
