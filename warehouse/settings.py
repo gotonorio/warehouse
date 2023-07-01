1cfee12aa626f3748d4338cd24b43e24667612c3
Goto Norio <gotonorio@gmail.com>
2023-07-01 15:18:12 +0900

    Macではヒラギノ角ゴシック、その他ではWebフォントのNoto Sans JPを使うように修正

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 CSRF_TRUSTED_ORIGINS = ['https://*.sophiagardens.org']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-VERSION_NO = '2023-05-08'
+VERSION_NO = '2023-07-01'
 MEMBERSHIP_FEE = 270
 # ファイルアップロードアプリuploder用
 # https://qiita.com/okoppe8/items/86776b8df566a4513e96
