0cd1e68a7dac7f6ddd3dbfd74b524d37f802765b
gotonorio <gotonorio@gmail.com>
2024-02-27 15:28:17 +0900

    djangoのバージョンアップ

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = "/static/"
 CSRF_TRUSTED_ORIGINS = ["https://*.sophiagardens.org"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
-VERSION_NO = "2023-07-01"
+VERSION_NO = "2024-02-27"
 MEMBERSHIP_FEE = 270
 # ファイルアップロードアプリuploder用
 # https://qiita.com/okoppe8/items/86776b8df566a4513e96
