36d44709070d29bc85e8a06664be67a0a20d1fb5
gotonorio <gotonorio@gmail.com>
2024-06-19 13:47:42 +0900

    マンション情報データ入力処理を見直した

--- information/forms.py
+++ information/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from information.models import Information
+from information.models import Information, InformationType
 
 
 class InformationForm(forms.ModelForm):
@@ -9,31 +9,42 @@ class InformationForm(forms.ModelForm):
     class Meta:
         model = Information
         fields = "__all__"
-        fields = ("title", "comment", "information", "display_info", "created_at")
+        fields = ("title", "comment", "information", "display_info", "created_at", "type_name")
         widgets = {
             "title": forms.TextInput(
                 attrs={
-                    "class": "input is-size-6",
+                    "class": "input",
                 }
             ),
             "comment": forms.TextInput(
                 attrs={
-                    "class": "input is-size-6",
+                    "class": "input",
                 }
             ),
             "information": forms.Textarea(
                 attrs={
-                    "class": "textarea is-size-6",
+                    "class": "textarea",
                 }
             ),
             "display_info": forms.CheckboxInput(
                 attrs={
-                    "class": "checkboxinput is-size-4",
+                    "class": "checkbox",
                 }
             ),
             "created_at": forms.DateTimeInput(
                 attrs={
-                    "class": "input is-size-6",
+                    "class": "input",
+                }
+            ),
+            "type_name": forms.Select(
+                attrs={
+                    "class": "select-css",
                 }
             ),
         }
+
+    def get_initial_for_field(self, field, field_name):
+        initial_id = InformationType.objects.get(type_name="情報")
+        if field_name == "type_name":
+            return initial_id
+        return super().get_initial_for_field(field, field_name)
