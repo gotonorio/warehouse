6b4bbc3d48add0c343c05b61506717e945b8eeea
gotonorio <gotonorio@gmail.com>
2025-07-04 13:36:18 +0900

    BASE_DIRをpath_libで設定するように修正

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import environ
 import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+import environ
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # instanceを作成
 env = environ.Env(
@@ -107,7 +109,7 @@ WSGI_APPLICATION = "warehouse.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, DB_NAME),
+        "NAME": BASE_DIR / DB_NAME,
     }
 }
 
