c5dfc508cfe8b6a4b1baaa9403156c69d03c64f1
gotonorio <gotonorio@gmail.com>
2026-01-27 11:06:58 +0900

    cssファイルの見直しとDockerfileの見直し

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -160,7 +160,7 @@ STATIC_URL = "/static/"
 CSRF_TRUSTED_ORIGINS = ["https://*.sophiagardens.org"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
-VERSION_NO = "2026-01-26"
+VERSION_NO = "2026-01-27"
 # ファイルアップロードアプリuploder用
 # https://qiita.com/okoppe8/items/86776b8df566a4513e96
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
